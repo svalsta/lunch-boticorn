86d3353355d55966171fa078aeb1258faf96295a
svalsta <svalsta@lm7-tike-11.local>
2018-04-07 11:20:17 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project template for Node.js
+
+Simple template for basic Node app setup
---
+++ src/index.js
@@ -0,0 +1,60 @@
+import { JSDOM } from 'jsdom';
+import request from 'request';
+
+import logger from './log';
+
+const menuUrl = 'http://pompier.fi/albertinkatu/lounas/';
+const weekdays = ['Maanantai', 'Tiistai', 'Keskiviikko', 'Torstai', 'Perjantai'];
+
+const parsePompierMenu = (html) => {
+  const dayIndex = new Date().getDay() - 1;
+  if (dayIndex < 0 || dayIndex >= weekdays.length) {
+    return 'Lunch available only weekdays!';
+  }
+
+  // Parse menu text
+  const jsdom = new JSDOM(html);
+  const cssSelector = '.page-content';
+  const menu = jsdom.window.document.querySelector(cssSelector).textContent;
+
+  // Find correct substring from menu
+  const startIndex = menu.indexOf(weekdays[dayIndex]);
+  const endIndex = dayIndex < weekdays.length - 1
+    ? menu.indexOf(weekdays[dayIndex + 1])
+    : menu.length;
+  if (startIndex >= 0 && startIndex < endIndex) {
+    const linesString = menu.substring(startIndex, endIndex);
+    const lines = linesString.split('\n');
+
+    // Format output
+    return [`<${menuUrl}|${lines[0]}>`, ...lines.slice(1, lines.length)].join('\n');
+  }
+
+  return 'Error fetching Pompier lunch information';
+};
+
+const fetchPompierMenu = async (url = menuUrl) => new Promise((resolve) => {
+  request(
+    url,
+    (error, { statusCode }, html) => {
+      if (!error && statusCode === 200) {
+        return resolve(parsePompierMenu(html));
+      }
+      const errorMsg = `Failed fetching Pompier menu: ${statusCode}`;
+      logger.error(errorMsg);
+      return resolve(errorMsg);
+    },
+  );
+});
+
+(async () => {
+  logger.info('Fetching Pompier menu for today...');
+  const text = await fetchPompierMenu();
+  logger.info(`Result:\n${text}`);
+  const payload = { text };
+  request({
+    uri: 'https://hooks.slack.com/services/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',
+    method: 'POST',
+    json: payload,
+  }, () => logger.info('Posted menu to slack!'));
+})();
